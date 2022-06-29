1261873af15182c9be278c6177bf6c39e24c972f
Harsh Baid <harshbaid.j@gmail.com>
2022-06-29 14:58:41 -0500

    Fix Stack to ConcurrentStack conversion code error

--- src/Foundation/ScribanExtensions/code/Cache/ScribanRenderCache.cs
+++ src/Foundation/ScribanExtensions/code/Cache/ScribanRenderCache.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Concurrent;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
 
 namespace Foundation.ScribanExtensions.Cache
 {
@@ -7,7 +6,7 @@ namespace Foundation.ScribanExtensions.Cache
     {
         private ConcurrentStack<string> _endFieldStack;
 
-        protected virtual Stack<string> EndFieldStack => _endFieldStack ?? (_endFieldStack = new Stack<string>());
+        protected virtual ConcurrentStack<string> EndFieldStack => _endFieldStack ?? (_endFieldStack = new ConcurrentStack<string>());
 
         public void PushEndFieldStack(string lastPart)
         {
